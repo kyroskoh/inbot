91ca8c9b4b4656ea5490bdab5f877fb49fa9b837
kotovaleksandr <kotov.a@hotmail.com>
2015-05-24 07:20:24 +0300

    options

--- options.py
+++ options.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-CLIENT_SECRET = ""
-ACCESS_TOKEN = ""
+__author__ = 'kotov.a'
 
-TAGS = ['cats', 'love']
-
+CLIENT_SECRET = "ef970c66160442808128e84246c00e4d"
+ACCESS_TOKEN = "1602650936.1fb234f.ebddd438216d41eb84c97f454b5cdddc"
+MAX_NO_FOLLOWED_BACK = 10000
+MAX_FOLLOWED_BACK = 10000
+CLEAN_LIMIT = 5000
+MAX_FOLLOW = 100
+TAGS = [u"макияж", u"макияжспб", u"визажист", "makeupartist", "visage", "makeup", "mua"]
